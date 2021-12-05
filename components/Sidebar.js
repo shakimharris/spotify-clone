d377f6a0b14584ddcc3b75ca2a7fc38dca7e5f86
Shakim Harris <amenraprod@gmail.com>
2021-12-05 07:24:17 -0500

    added spacing

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,21 +1,35 @@
-import { HomeIcon, SearchIcon, LibraryIcon } from '@heroicons/react/outline';
+import { HomeIcon, SearchIcon, LibraryIcon, HeartIcon, RssIcon, PlusCircleIcon } from '@heroicons/react/outline';
 
 function Sidebar() {
     return (
-        <div className="text-gray-500 p-5 x-sm">
-        <div>
+        <div className="text-gray-500 p-5 x-sm border-r border-gray-900">
+        <div className="space-y-4">
             <button className="flex items-center space-x-2 hover:text-white">
-                <HomeIcon className="h-5 w-5" />
-                <p>Home</p>
+            <HomeIcon className="h-5 w-5" />
+            <p>Home</p>
             </button>
              <button className="flex items-center space-x-2 hover:text-white">
-                 <SearchIcon className="h-5 w-5" />
-                 <p>Search</p>
+            <SearchIcon className="h-5 w-5" />
+            <p>Search</p>
              </button>
              <button className="flex items-center space-x-2 hover:text-white">
-                 <LibraryIcon className="h-5 w-5" />
-                 <p>Library</p>
+            <LibraryIcon className="h-5 w-5" />
+            <p>Library</p>
              </button>
+             <hr className="border-t-[0.1px]" />
+             <button className="flex items-center space-x-2 hover:text-white">
+            <PlusCircleIcon className="h-5 w-5" />
+            <p>Create a playlist</p>
+             </button>
+             <button className="flex items-center space-x-2 hover:text-white">
+            <HeartIcon className="h-5 w-5" />
+            <p>Liked songs</p>
+             </button>
+             <button className="flex items-center space-x-2 hover:text-white">
+            <RssIcon className="h-5 w-5" />
+            <p>Your episodes</p>
+             </button>
+             
         </div>
     </div>
     )
